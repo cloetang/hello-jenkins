99be150a534db4195272c2bd2eb08523d6dce4b6
cloe.tang <hui.min.tang@accenture.com>
2016-01-27 18:21:40 +0800

    Edited test.bat

--- script/test.bat
+++ script/test.bat
@@ -1,3 +1,1 @@
-#!/bin/sh
- 
-C:/Users\hui.min.tang\Documents\JenkinsDemo\hello-jenkins\node_modules\.bin\mocha.cmd C:/Users\hui.min.tang\Documents\JenkinsDemo\hello-jenkins\test\test.js
+/node_modules\.bin\mocha.cmd test\test.js
